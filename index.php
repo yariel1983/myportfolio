f47c7617d11f71896b5eb81b7dd1b8b010dd9b60
Yariel1983 <cubanboy4life@gmail.com>
2019-01-17 22:09:56 -0500

    2 commit

--- index.php
+++ index.php
@@ -33,61 +33,61 @@
 
                         <h6>HTML, HTML5, BOOTSTRAP, CSS, CSS3, SASS, SCSS, REACT.JSX, REACT.JS INSPECTOR, AJAX, FETCH, RESTful API, JR. JAVA SCRIPT, VANILLA JS, NPM, NVM, NPM RUN BUILD, NPM RUN C9, WEB SERVICES, WEB CONSOLE & INSPECTOR, DEBUG, LINUX TERMINAL, APACHE SERVER, COMMAND LINE, GITHUB REPOSITORY, BREATHECO WEBPACK, FONT AWESOME, WORDPRESS PLUGINS WOOCOMMERCE, JETPACK, ACF, PHP, CLOUD9 IDE EXPERT, PROBLEM SOLVER..</h6>
                         <div class="container">
-                        <!-- Grid row-->
-                        <div class="row">
+                            <!-- Grid row-->
+                            <div class="row">
 
-                            <!-- Grid column -->
-                            <div class="col-md-12 py-5">
-                                <div class="mb-5 flex-center">
+                                <!-- Grid column -->
+                                <div class="col-md-12 py-5">
+                                    <div class="mb-5 flex-center">
 
-                                    <!-- Facebook -->
-                                    <a class="fb-ic">
-                                        <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
-                                    </a>
-                                    <!-- Twitter -->
-                                    <a class="tw-ic">
-                                        <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
-                                    </a>
-                                    <!-- Google +-->
-                                    <a class="gplus-ic">
-                                        <i class="fab fa-github fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
-                                    </a>
-                                    <!--Linkedin -->
-                                    <a class="li-ic">
-                                        <i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
-                                    </a>
-                                    
-                                  
-
+                                        <!-- Facebook -->
+                                        <a class="fb-ic">
+                                            <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                                        </a>
+                                        <!-- Twitter -->
+                                        <a class="tw-ic">
+                                            <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                                        </a>
+                                        <!-- Google +-->
+                                        <a class="gplus-ic">
+                                            <i class="fab fa-github fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                                        </a>
+                                        <!--Linkedin -->
+                                        <a class="li-ic">
+                                            <i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                                        </a>
+                                    </div>
                                 </div>
+                                <!-- Grid column -->
+
                             </div>
-                            <!-- Grid column -->
-
+                            <!-- Grid row-->
                         </div>
-                        <!-- Grid row-->
                 </div>
-            </div>
-        </div>	
+            </div>	
+        </div>
     </div>
-</div>
-<!-- Footer -->
-<footer class="page-footer font-small cyan darken-3">
+    <!-- Footer -->
+    <footer class="page-footer font-small cyan darken-3">
 
-    <!-- Footer Elements -->
-    <div class="container">
+        <!-- Footer Elements -->
+        <div class="container">
+        </div>
+        <!-- Footer Elements -->
+
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3">© 2019 Copyright:
+
+            <a href="https://github.com/yariel1983"><i class="fab fa-github fa-2x"></i></a>
+
+        </div>
+        <!-- Copyright -->
+
+    </footer>
+    <!-- Footer -->
 
 
 
-    </div>
-    <!-- Footer Elements -->
 
-    <!-- Copyright -->
-    <div class="footer-copyright text-center py-3">© 2019 Copyright:
-        
-        <a href="https://github.com/yariel1983"><i class="fab fa-github fa-2x"></i></a>
 
-    </div>
-    <!-- Copyright -->
 
-</footer>
-<!-- Footer -->
